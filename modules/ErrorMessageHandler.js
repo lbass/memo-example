3033476d423da4af655abfbd87baaad18a59be63
Lbass <ehdnjsdl0929@gmail.com>
2017-03-14 17:50:39 +0900

    에러 코드 추가

--- modules/ErrorMessageHandler.js
+++ modules/ErrorMessageHandler.js
@@ -1,4 +1,10 @@
 const ErrorCodeSet = {
+    'account.001': { httpCode: 403, message: 'bad username'},
+    'account.002': { httpCode: 403, message: 'bad password'},
+    'account.003': { httpCode: 403, message: 'username exists'},
+    'account.004': { httpCode: 403, message: 'password error'},
+    'account.005': { httpCode: 401, message: 'login failed'},
+    'account.006': { httpCode: 401, message: 'not found login user'},
     'memo.001': { httpCode: 403, message: 'not login user' },
     'memo.002': { httpCode: 403, message: 'contents is empty' },
     'memo.003': { httpCode: 403, message: 'not found resource' },
